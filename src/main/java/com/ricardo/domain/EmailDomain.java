56f2c085117f434ce89bc3a6aa0e3150ade7b470
Ricardo <ricardofps66@gmail.com>
2022-05-22 18:09:38 -0300

    Creating Email Database

--- src/main/java/com/ricardo/domain/EmailDomain.java
+++ src/main/java/com/ricardo/domain/EmailDomain.java
@@ -1,10 +1,15 @@
 package com.ricardo.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.GenericGenerator;
+
+import com.ricardo.enums.StatusEmail;
 
 @Entity
 public class EmailDomain {
@@ -14,4 +19,14 @@ public class EmailDomain {
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
+	private String ownerRef;
+	private String emailFrom;
+	private String emailTo;
+	private String subject;
+	@Column(columnDefinition = "TEXT")
+	private String text;
+	private LocalDateTime sendDateEmail;
+	private StatusEmail statusEmail;
+	
+	
 }
