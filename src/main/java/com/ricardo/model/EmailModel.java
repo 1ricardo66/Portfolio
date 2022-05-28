e59d3820a1781f05e641e499e400face4532f48e
Ricardo <ricardofps66@gmail.com>
2022-05-28 11:17:38 -0300

    Appling Email Function

--- src/main/java/com/ricardo/model/EmailModel.java
+++ src/main/java/com/ricardo/model/EmailModel.java
@@ -1,32 +1,127 @@
 package com.ricardo.model;
-
+import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.hibernate.annotations.GenericGenerator;
+import javax.persistence.Table;
 
 import com.ricardo.enums.StatusEmail;
 
-@Entity
-public class EmailModel {
 
+
+@Entity
+@Table(name = "TB_EMAIL")
+public class EmailModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private UUID emailId;
+    private String ownerRef;
+    private String emailFrom;
+    private String emailTo;
+    private String subject;
+    @Column(columnDefinition = "TEXT")
+    private String text;
+    private LocalDateTime sendDateEmail;
+    private StatusEmail statusEmail;
+    
+    
+    
+    
+    
+	public EmailModel() {
+		super();
+	}
+	public EmailModel(UUID emailId, String ownerRef, String emailFrom, String emailTo, String subject, String text,
+			LocalDateTime sendDateEmail, StatusEmail statusEmail) {
+		super();
+		this.emailId = emailId;
+		this.ownerRef = ownerRef;
+		this.emailFrom = emailFrom;
+		this.emailTo = emailTo;
+		this.subject = subject;
+		this.text = text;
+		this.sendDateEmail = sendDateEmail;
+		this.statusEmail = statusEmail;
+	}
+	public UUID getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(UUID emailId) {
+		this.emailId = emailId;
+	}
+	public String getOwnerRef() {
+		return ownerRef;
+	}
+	public void setOwnerRef(String ownerRef) {
+		this.ownerRef = ownerRef;
+	}
+	public String getEmailFrom() {
+		return emailFrom;
+	}
+	public void setEmailFrom(String emailFrom) {
+		this.emailFrom = emailFrom;
+	}
+	public String getEmailTo() {
+		return emailTo;
+	}
+	public void setEmailTo(String emailTo) {
+		this.emailTo = emailTo;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public LocalDateTime getSendDateEmail() {
+		return sendDateEmail;
+	}
+	public void setSendDateEmail(LocalDateTime sendDateEmail) {
+		this.sendDateEmail = sendDateEmail;
+	}
+	public StatusEmail getStatusEmail() {
+		return statusEmail;
+	}
+	public void setStatusEmail(StatusEmail statusEmail) {
+		this.statusEmail = statusEmail;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(emailFrom, emailId, emailTo, ownerRef, sendDateEmail, statusEmail, subject, text);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EmailModel other = (EmailModel) obj;
+		return Objects.equals(emailFrom, other.emailFrom) && Objects.equals(emailId, other.emailId)
+				&& Objects.equals(emailTo, other.emailTo) && Objects.equals(ownerRef, other.ownerRef)
+				&& Objects.equals(sendDateEmail, other.sendDateEmail) && statusEmail == other.statusEmail
+				&& Objects.equals(subject, other.subject) && Objects.equals(text, other.text);
+	}
 	
-	@Id
-	@GeneratedValue(generator = "increment")
-	@GenericGenerator(name = "increment", strategy = "increment")
-	private Long id;
-	private String ownerRef;
-	private String emailFrom;
-	private String emailTo;
-	private String subject;
-	@Column(columnDefinition = "TEXT")
-	private String text;
-	private LocalDateTime sendDateEmail;
-	private StatusEmail statusEmail;
-	
-	
+    
+    
+    
 }
