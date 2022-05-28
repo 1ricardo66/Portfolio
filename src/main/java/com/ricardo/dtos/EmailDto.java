3db33f95273ec5b1137df2c4c342ca9f8f161022
Ricardo <ricardofps66@gmail.com>
2022-05-28 15:58:32 -0300

    Updating DTO

--- src/main/java/com/ricardo/dtos/EmailDto.java
+++ src/main/java/com/ricardo/dtos/EmailDto.java
@@ -21,36 +21,69 @@ public class EmailDto {
     private String subject;
     //@NotBlank
     private String text;
+    
+    private String phone;
+    
+    private String sender;
+
 	public String getOwnerRef() {
 		return ownerRef;
 	}
+
 	public void setOwnerRef(String ownerRef) {
 		this.ownerRef = ownerRef;
 	}
+
 	public String getEmailFrom() {
 		return emailFrom;
 	}
+
 	public void setEmailFrom(String emailFrom) {
 		this.emailFrom = emailFrom;
 	}
+
 	public String getEmailTo() {
 		return emailTo;
 	}
+
 	public void setEmailTo(String emailTo) {
 		this.emailTo = emailTo;
 	}
+
 	public String getSubject() {
 		return subject;
 	}
+
 	public void setSubject(String subject) {
 		this.subject = subject;
 	}
+
 	public String getText() {
 		return text;
 	}
+
 	public void setText(String text) {
 		this.text = text;
 	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+    
+	
+    
     
     
     
